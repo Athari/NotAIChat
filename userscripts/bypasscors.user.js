039ef97b592fd58de00a207fc9d173ce6e946d3b
Athari <athari.p@gmail.com>
2023-03-20 22:55:38 +0300

    Added support for sending one message with retries. Fixed generation of first message's id. Fixed typo in proxy logic. Disabled web proxy just in case.

--- userscripts/bypasscors.user.js
+++ userscripts/bypasscors.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
-// @name         Scale Chat - CORS
+// @name         Scale Chat - Bypass CORS
 // @version      1.0
-// @description  Bypass CORS
+// @description  Bypass CORS in Scale Chat app. Add extra matches to enable on other domains.
 // @author       Athari
 // @match        https://athari.github.io/ScaleChat/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=scale.com
 // @grant        GM_xmlhttpRequest
 // @grant        unsafeWindow
+// @connect      dashboard.scale.com
+// @connect      *
 // @run-at       document-start
 // @require      https://raw.githubusercontent.com/mitchellmebane/GM_fetch/master/GM_fetch.min.js
 // ==/UserScript==
