6b737826d449826de2cb2d564ba5dc6bb76cffb0
Athari <athari.p@gmail.com>
2023-03-21 18:19:26 +0300

    Changed building into single HTML with inline JS and CSS.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const SveltePreprocess = require('svelte-preprocess');
+const webpack = require('webpack');
+const { Compilation } = webpack;
 
 const cssHashMap = { App: 'a', Fa: 'f' };
 
@@ -17,6 +19,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
     publicPath: './',
+    clean: true,
   },
   module: {
     rules: [
@@ -77,6 +80,7 @@ module.exports = {
     //Allows to create an index.html in our build folder 
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
+      inject: 'body',
     }),
     //This gets all our css and put in a unique file
     new MiniCssExtractPlugin({
@@ -85,6 +89,39 @@ module.exports = {
     //take our environment variable in .env file
     //And it does a text replace in the resulting bundle for any instances of process.env.
     new Dotenv(),
+    // Incline CSS and JS
+    {
+      apply(compiler) {
+        const pluginName = 'InlineCode';
+        compiler.hooks.compilation.tap(pluginName, compilation => {
+          compilation.hooks.processAssets.tapPromise(
+            { name: pluginName, stage: Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_INLINE },
+            async assets => {
+              for (const [ name, source ] of Object.entries(assets)) {
+                if (name.match(/\.html$/)) {
+                  let html = source.source().toString();
+                  let m;
+                  while ((m = /<script.*?src="\.\/([^"]+)"><\/script>/i.exec(html)) != null) {
+                    const [ scriptName, start, end ] = [ m[1], m.index, m.index + m[0].length ];
+                    const inlineScript = assets[scriptName].source().toString().trim();
+                    html = `${html.substring(0, start)}<script>${inlineScript}</script>${html.substring(end)}`;
+                    compilation.deleteAsset(scriptName);
+                  }
+                  while ((m = /<link.*?href="\.\/([^"]+)" rel="stylesheet">/i.exec(html)) != null) {
+                    const [ styleName, start, end ] = [ m[1], m.index, m.index + m[0].length ];
+                    const inlineStyle = assets[styleName].source().toString().trim();
+                    html = `${html.substring(0, start)}<style>${inlineStyle}</style>${html.substring(end)}`;
+                    compilation.deleteAsset(styleName);
+                  }
+                  html = html.replace(/ +/ig, " ");
+                  compilation.updateAsset(name, new webpack.sources.RawSource(html));
+                }
+              }
+            }
+          )
+        })
+      },
+    }
   ],
   optimization: {
     minimize: true,
